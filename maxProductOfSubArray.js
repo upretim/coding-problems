c187106003f1fded064682689ef8206ef4c09723
upretim <manojupreti83@yahoo.co.in>
2021-02-07 19:00:53 +0530

    Added new solution

---
+++ maxProductOfSubArray.js
@@ -0,0 +1,19 @@
+//https://leetcode.com/problems/maximum-product-subarray/
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxProduct = function(nums) {
+    let maxproductArray = nums[0];
+    let max = nums[0]
+     let min = nums[0]
+    for(let i=1;i<nums.length;i++){
+            let temp = Math.max(nums[i], max*nums[i], min*nums[i]);
+            min = Math.min(nums[i], max*nums[i], min*nums[i]);
+            max = temp;
+           if(max>maxproductArray) maxproductArray = max;        
+    }
+    return maxproductArray;
+};
+maxProduct([2,3,-2,4]);
