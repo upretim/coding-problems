9c17fe696110ced96e441962e2c9183871fe8fc5
upretim <manojupreti83@yahoo.co.in>
2021-03-21 20:02:25 +0530

    added memoization

--- dynamic programming/targetSum.js
+++ dynamic programming/targetSum.js
@@ -4,15 +4,22 @@
  * we can use a number multiple time and array contains non negative numbers only.
  */
 
-const canSum=(target, array)=>{
+const canSum=(target, array, memo={})=>{
+    if(target in memo) return memo[target];
     if(target==0) return true;
     if(target<0) return false;
      for(let num of array){
          const remender = target-num;
-        if(canSum(remender, array)===true) return true;
+        if(canSum(remender, array, memo)===true) {
+            memo[target] = true;
+            return true;
+        }
+       
      }
-  return false;
+    memo[target] = false;
+    return false;
 }
 
 console.log(canSum(7,[4,2]));
-console.log(canSum(7,[4,3]))
+console.log(canSum(7,[4,3]));
+console.log(canSum(300,[7,14]));
