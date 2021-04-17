e36108c5803d93fc00a421bc43825d2efae0c49e
upretim <manojupreti83@yahoo.co.in>
2021-04-17 13:11:26 +0530

    added stack solution

--- remove-all-adjacent-duplicates.js
+++ remove-all-adjacent-duplicates.js
@@ -32,4 +32,25 @@ var removeDuplicates = function(s, k) {
      return sReplaced;   
  };
 
+ var removeDuplicatesUsingStack = function(s, k) {
+
+    let stack = [[s.charAt(0),1]];
+    for(let i=1; i<s.length; i++) {
+        let last = stack.length-1;
+       if((last >=0) && stack[last][0]==s.charAt(i)){
+         stack[last][1]++;
+         if(stack[last][1]==k) stack.pop();
+       }
+       else{
+           stack.push([s.charAt(i), 1])
+       }
+    }
+     let str = "";
+     for(let i=0;i<stack.length;i++){
+        str = str +stack[i][0].repeat(stack[i][1])
+     }
+     return str;
+};
+
+
 removeDuplicates("deeedbbcccbdaa", 3);
